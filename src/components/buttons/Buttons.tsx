8ef189c41c5309e1245fda9afe066e0563233f68
Angelus Trindade <angelusrt@gmail.com>
2023-02-04 19:49:11 -0300

    🕹 Novos botões adicionados

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -2,21 +2,34 @@ import { Text } from "../texts/Texts"
 import "./buttons.css"
 
 const textName = {
-  'button-black':'text-button-white', 
-  'button-white':'text-button-black'
+  'black':'text-button-white', 
+  'white':'text-button-black'
 }
-type buttonProp = {
-  name: 'button-white' | 'button-black',
+const buttonName = {
+  'white':'button-white', 
+  'black':'button-black',
+  'h1': 'button-title',
+  'h2': ''
+}
+
+type buttonProps = {
+  color: 'white' | 'black',
   type: 'h1' | 'h2',
   text: string,
-  children ?: any
+  children ?: any,
+  func ?: any
 } 
 
-function Button(props: buttonProp): JSX.Element{
+function Button(props: buttonProps): JSX.Element{
+  const name = (buttonName[props.color] + " " + buttonName[props.type])
+  
   return(
-    <button className={props.name}>
+    <button 
+      className={name} 
+      {...props.func}
+    >
       <Text 
-        name={textName[props.name]}
+        name={textName[props.color]}
         type={props.type}
       >
         {props.text}
@@ -26,4 +39,25 @@ function Button(props: buttonProp): JSX.Element{
   )
 }
 
-export{Button}
+type linkProps = {
+  color: 'white' | 'black',
+  text: string,
+  href: string
+} 
+
+function Link(props:linkProps): JSX.Element{
+  const name = buttonName[props.color]
+
+  return(
+    <a className={name} href={props.href}>
+      <Text 
+        name={textName[props.color]}
+        type='h2'
+      >
+        {props.text}
+      </Text>
+    </a>
+  )
+}
+
+export{Button, Link}
