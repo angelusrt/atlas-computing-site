898479469aa91f0794a08e4bd47ae1a68bae8100
Angelus Trindade <angelusrt@gmail.com>
2023-05-16 14:25:09 -0300

    🥸 Icone do botão animado

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -29,15 +29,10 @@ const Dropdown = () => (
   </svg>
 )
 const Menu = () => (
-  <svg className="icon svg-menu" viewBox="0 30 448 440">
-    <path 
-      d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 
-      32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 
-      256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 
-      32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 
-      416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 
-      32-32H416c17.7 0 32 14.3 32 32z" 
-    />
+  <svg className="icon svg-menu" viewBox="0 0 100 100" style={iconStyle}>
+    <rect className="top" x="0" y="10" width="100" height="10" fill="var(--black)"/>
+    <rect className="middle" x="0" y="45" width="100" height="10" fill="var(--black)"/>
+    <rect className="bottom" x="0" y="80" width="100" height="10" fill="var(--black)"/>
   </svg>
 )
 const Recife = () => (
